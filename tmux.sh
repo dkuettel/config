3fe7436abd4e1cc16b2ba15485040c4c2c7e08f1
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 14:04:32 +0200

    tmux

--- tmux.sh
+++ tmux.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt-get install tmux
 ln -s --backup ~/config/tmux.conf ~/.tmux.conf
+mkdir -p ~/plugins/tmux
+cd ~/plugins/tmux
+git clone git@github.com:dkuettel/tmux-colors-solarized.git
+cd tmux-colors-solarized
+git config user.email 'dkuettel@gmail.com'
+git remote add upstream https://github.com/seebi/tmux-colors-solarized.git
+git checkout daniel
