9d0b8d9e1a35e81bbc6db11447fad8e1b5042b3a
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 16:27:54 +0200

    zsh

---
+++ zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# chsh -s /bin/zsh # seems like oh-my-zsh is doing that already
