f068cd896bf94722e96ebdf0dda2ad3793ce0686
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 10:00:30 +0200

    install package

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install git
 git config --global user.email "kuettel@qce.qualcomm.com"
 git config --global user.name "Daniel Kuettel"
 git config --global push.default simple
