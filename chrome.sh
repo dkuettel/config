d070b3963134ecccd5523ccc8267dd0a5df57475
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 10:32:28 +0200

    install chrome

---
+++ chrome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get -f install libindicator7 libappindicator1 # in ubuntu 14.04 this dependency was missing
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
