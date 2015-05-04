0d99c2cd4f550fa0cc4480f34ea288490aa63c00
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 16:09:35 +0200

    add more color settings

--- gnome-terminal.sh
+++ gnome-terminal.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+# gnome terminal solarized theme
 mkdir -p ~/plugins
 cd ~/plugins
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 sudo apt-get install dconf-cli
 ./install.sh
+# or maybe set_light.sh ?
+
+# ls colors
+mkdir -p ~/plugins
+cd ~/plugins
+git clone git@github.com:dkuettel/dircolors-solarized.git
+cd dircolors-solarized
+git config user.email 'dkuettel@gmail.com'
+git remote add upstream https://github.com/seebi/dircolors-solarized.git
+git checkout mine
