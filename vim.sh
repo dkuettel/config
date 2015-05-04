17672ad0fc8d75b730c52bfd80622d32ed8b4e89
Daniel Kuettel <dkuettel@gmail.com>
2015-05-04 19:36:57 +0200

    remove powerline from vim installation

--- vim.sh
+++ vim.sh
@@ -10,5 +10,3 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
 
-sudo apt-get install python-pip python-setuptools
-pip install --user powerline-status
