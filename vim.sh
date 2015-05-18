41ecd3a82e10a4e0143e28b5b1e0a2d589b12041
Daniel Kuettel <dkuettel@gmail.com>
2015-05-18 16:17:18 +0200

    vim

--- vim.sh
+++ vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install vim
+sudo apt-get install vim-nox
 ln -s --backup ~/config/vimrc ~/.vimrc
 
 # pathogen
@@ -10,3 +10,10 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
 
+# nerdtree
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+# lusty juggler and file explorer
+cd ~/.vim/bundle
+git clone https://github.com/sjbach/lusty.git
