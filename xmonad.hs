a867ef7a91d48ac17bdaf0e73c7b6715aa76b0e0
Daniel Kuettel <dkuettel@gmail.com>
2015-05-28 15:44:30 +0200

    fix focus issue with chrome and others in xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,18 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
 main = do
-	xmonad $ defaultConfig
+	xmonad $ ewmh defaultConfig
 		{ manageHook = manageDocks <+> manageHook defaultConfig
 		, layoutHook = avoidStruts $ layoutHook defaultConfig
 		, modMask = mod4Mask -- bind mod to windows key (default was alt)
 		, borderWidth = 4
+		, handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook -- chrome and others don't support proper x11 anymore
 		} `additionalKeysP`
 		[ ("<F8>",spawn "/home/kuettel/playerctl/playerctl/playerctl play-pause")
 		, ("<F9>",spawn "/home/kuettel/playerctl/playerctl/playerctl next")
