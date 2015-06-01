618a83c46190c5c9764682a181dd0a9a4e960dc7
Daniel Kuettel <dkuettel@gmail.com>
2015-06-01 08:09:22 +0000

    vim idempotent

--- vim.sh
+++ vim.sh
@@ -1,31 +1,79 @@
 #!/bin/bash
 
-sudo apt-get install vim-nox
-ln -s --backup ~/config/vimrc ~/.vimrc
+# install packages
+sudo apt-get -q install vim-nox
+
+if [[ $(realpath ~/config/vimrc) == $(realpath ~/.vimrc) ]]
+then
+	echo '.vimrc is already configured'
+else
+	echo 'backup old .vimrc'
+	ln -s --backup ~/config/vimrc ~/.vimrc
+fi
 
 # pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+if [[ ! -f ~/.vim/autoload/pathogen.vim ]]
+then
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
 
+# solarized colors
 cd ~/.vim/bundle
-git clone git://github.com/altercation/vim-colors-solarized.git
+if [[ -d vim-colors-solarized ]]
+then
+	cd vim-colors-solarized
+	git pull
+else
+	git clone git://github.com/altercation/vim-colors-solarized.git
+fi
 
 # nerdtree
 cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
+if [[ -d nerdtree ]]
+then
+	cd nerdtree
+	git pull
+else
+	git clone https://github.com/scrooloose/nerdtree.git
+fi
 
 # lusty juggler and file explorer
 cd ~/.vim/bundle
-git clone https://github.com/sjbach/lusty.git
+if [[ -d lusty ]]
+then
+	cd lusty
+	git pull
+else
+	git clone https://github.com/sjbach/lusty.git
+fi
 
 # easy motion
 cd ~/.vim/bundle
-git clone https://github.com/Lokaltog/vim-easymotion.git
+if [[ -d vim-easymotion ]]
+then
+	cd vim-easymotion
+	git pull
+else
+	git clone https://github.com/Lokaltog/vim-easymotion.git
+fi
 
 # auto save
 cd ~/.vim/bundle
-git clone https://github.com/907th/vim-auto-save.git
+if [[ -d vim-auto-save ]]
+then
+	cd vim-auto-save
+	git pull
+else
+	git clone https://github.com/907th/vim-auto-save.git
+fi
 
 # ctrl p
 cd ~/.vim/bundle
-git clone https://github.com/kien/ctrlp.vim.git
+if [[ -d ctrlp.vim ]]
+then
+	cd ctrlp.vim
+	git pull
+else
+	git clone https://github.com/kien/ctrlp.vim.git
+fi
