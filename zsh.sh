1d903579a7736ea884576da14fc1dd37041c5a61
Daniel Kuettel <dkuettel@gmail.com>
2015-06-09 17:36:10 +0200

    correct zsh path

--- zsh.sh
+++ zsh.sh
@@ -8,7 +8,7 @@ then
 	echo 'oh my zsh is already installed'
 else
 	wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-	chsh -s /bin/zsh # seems like oh-my-zsh is doing that already, sometimes?
+	chsh -s /usr/bin/zsh # seems like oh-my-zsh is doing that already, sometimes?
 	ln -s --backup ~/config/zshrc ~/.zshrc
 fi
 
