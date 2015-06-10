22c4c543916ed82ae043b251d4c42e2e161c19c2
Daniel Kuettel <dkuettel@gmail.com>
2015-06-10 14:49:23 +0200

    bose bluetooth

---
+++ bin/bose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bluez-test-audio connect 00:0C:8A:69:3B:C4
+pactl list cards short
+pactl set-default-sink bluez_card.00_0C_8A_69_3B_C4
+
+# notes:
+# also needed to Disable=Headset in section [General] in /etc/bluetooth/audio.conf
+# and sudo restart bluetooth
+# otherwise it's in headset mode and the mic and sound share bandwidth
+# maybe also pactl set-card-profile x a2dp
