20d3553616ae730f300283c8de78e8012c84da6b
Daniel Kuettel <dkuettel@gmail.com>
2015-11-09 11:39:50 +0100

    new email

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 set -ex
 
 sudo apt-get install -y git
-git config --global user.email "kuettel@qce.qualcomm.com"
+git config --global user.email "dkuettel@ptc.com"
 git config --global user.name "Daniel Kuettel"
 git config --global push.default simple
 git config --global core.editor "vim"
