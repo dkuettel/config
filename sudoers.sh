6bca44e9d12c4bae15db40a769feae80ac7a1df1
Daniel Kuettel <dkuettel@gmail.com>
2016-01-20 17:38:21 +0100

    add link

--- sudoers.sh
+++ sudoers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xeu
 # copy the sudoers file over to be in effect (symlink not possible)
+# see http://askubuntu.com/questions/159007/how-do-i-run-specific-sudo-commands-without-a-password
 
 sudo cp sudoers /etc/sudoers.d/kuettel
 
