4e231e5a4b63dfa659365de28aa3d867f60519be
Daniel Kuettel <dkuettel@gmail.com>
2016-08-17 16:49:52 +0200

    remove some stuff and moved to nn/bin of nn project

--- bin/some_swap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# assuming on /mnt, for amazon ubuntu that's usually the local (ephemeral) ssd
-cd /mnt
-sudo dd if=/dev/zero of=swapfile bs=1G count=4
-sudo chmod 600 swapfile
-sudo mkswap swapfile
-sudo swapon swapfile
-swapon -s
