91061d89e2a38af433e61cd7b9d3e02512ed5efd
Daniel Kuettel <dkuettel@gmail.com>
2016-10-24 16:39:29 +0200

    adapt for new gpu

--- bin/mount_typical.sh
+++ bin/mount_typical.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -xeu
+#!/bin/bash -eux
 set -o pipefail
 sudo mount /dev/xvdf /data/xps/metric/dev
-sudo mount /dev/xvdg /home/kuettel/web
-sudo mkdir /mnt/sets
-sudo chown kuettel:kuettel /mnt/sets
+# todo how to find out if we are on a machine with /mnt as ephemeral?
+#sudo mkdir /mnt/sets
+#sudo chown kuettel:kuettel /mnt/sets
