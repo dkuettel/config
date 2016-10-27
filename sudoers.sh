84ccd31cf08c6894d95d7c0e610e0f736aafec34
Daniel Kuettel <dkuettel@gmail.com>
2016-10-27 16:59:46 +0200

    remove sudoers, it is now in nn by default

--- sudoers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xeu
-# copy the sudoers file over to be in effect (symlink not possible)
-# see http://askubuntu.com/questions/159007/how-do-i-run-specific-sudo-commands-without-a-password
-
-sudo cp sudoers /etc/sudoers.d/kuettel
-
-echo 'dont forget sudoers needs to include the sudoers.d folder, edit with visudo, remember hostname needs to be staging for it to have an effect'
