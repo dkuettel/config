876ec4d7bae9f35e2033b1f9e34c720ce9bf8864
Daniel Kuettel <dkuettel@gmail.com>
2016-10-27 16:33:38 +0200

    config ipython for exit

---
+++ python.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xeu
+set -o pipefail
+
+sudo apt-get install -yq python python-pip build-essential
+sudo -H pip install --upgrade pip
+
+ipython profile create
+sed -i 's/#c.TerminalInteractiveShell.confirm_exit = True/c.TerminalInteractiveShell.confirm_exit = False/' ~/.ipython/profile_default/ipython_config.py
