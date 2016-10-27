d7a0397b608e2a8ed43fa6ee2cf50a674d1028b2
Daniel Kuettel <dkuettel@gmail.com>
2016-10-27 16:33:50 +0200

    add typical mounts

--- bin/mount_typical.sh
+++ bin/mount_typical.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eux
 set -o pipefail
 sudo mount /dev/xvdf /data/xps/metric/dev
-# todo how to find out if we are on a machine with /mnt as ephemeral?
-#sudo mkdir /mnt/sets
-#sudo chown kuettel:kuettel /mnt/sets
+sudo mount /dev/xvdg /data/sets
+sudo mount /dev/xvdh1 ~/old_root
