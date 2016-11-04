4b9993665390cd99195c261ab7e2bab4e8d2af9c
Daniel Kuettel <dkuettel@gmail.com>
2016-11-04 15:49:30 +0100

    adapt resolution

--- bin/vnc_office.sh
+++ bin/vnc_office.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xeu
 vncserver -kill :1 -clean || echo 'there was no previous x'
-vncserver -geometry 1920x1080 # desktop
+#vncserver -geometry 1920x1080 # desktop
+vncserver -geometry 1900x1060 # desktop, a bit of slack on the sides to fit with window borders
