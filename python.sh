88d4510712564430d85e2d5c0276772f3ab806bf
Daniel Kuettel <dkuettel@gmail.com>
2016-11-18 15:47:00 -0800

    be sure to have it installed and fix regexp for config

--- python.sh
+++ python.sh
@@ -4,5 +4,6 @@ set -o pipefail
 sudo apt-get install -yq python python-pip build-essential
 sudo -H pip install --upgrade pip
 
+sudo apt-get install -yq ipython
 ipython profile create
-sed -i 's/#c.TerminalInteractiveShell.confirm_exit = True/c.TerminalInteractiveShell.confirm_exit = False/' ~/.ipython/profile_default/ipython_config.py
+sed -i 's/# c.TerminalInteractiveShell.confirm_exit = True/c.TerminalInteractiveShell.confirm_exit = False/' ~/.ipython/profile_default/ipython_config.py
