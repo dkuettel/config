5f6e9fa1561aead95df769ba89853dbb8309465a
Daniel Kuettel <dkuettel@gmail.com>
2016-11-19 17:45:04 -0800

    add todos

--- setup_with_head.sh
+++ setup_with_head.sh
@@ -3,6 +3,11 @@ set -o pipefail
 
 echo 'assumes headless already setup'
 
+# todo keyboard layout
+# todo xmoand
+# todo spotify and shortcuts
+# todo dropbox?
+
 ./fonts.sh
 ./gnome-terminal.sh
 ./chrome.sh
