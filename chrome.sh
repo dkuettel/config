f15550fe04aa9f662aa1bce7934e700c930ad7d6
Daniel Kuettel <dkuettel@gmail.com>
2016-11-29 10:02:39 +0100

    use yes by default

--- chrome.sh
+++ chrome.sh
@@ -5,7 +5,7 @@ if which google-chrome; then
 	echo 'google-chrome already installed'
 else
 	wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome.deb
-	sudo apt-get -f install libindicator7 libappindicator1 libpango1.0-0 libpangox-1.0-0
+	sudo apt-get -fyq install libindicator7 libappindicator1 libpango1.0-0 libpangox-1.0-0
 	sudo dpkg -i google-chrome.deb
 	rm google-chrome.deb
 fi
