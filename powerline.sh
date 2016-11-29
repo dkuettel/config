f96e6c0494b0a1146750b50a1696aa2eaa12d759
Daniel Kuettel <dkuettel@gmail.com>
2016-11-29 09:53:32 +0100

    use more accessible https protocol

--- powerline.sh
+++ powerline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # install latest git version (to work with latest tmux)
-sudo -H pip install --user git+git://github.com/powerline/powerline
+sudo -H pip install --user git+https://github.com/powerline/powerline
 
 [[ -d ~/.config ]] || mkdir -p ~/.config
 if [[ $(realpath ~/config/powerline) != $(realpath ~/.config/powerline) ]]; then
