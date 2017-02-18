d0b5de2b18513fe6dd12b58f9d0efec14266a92c
Daniel Kuettel <dkuettel@gmail.com>
2017-02-18 13:52:00 +0100

    adapt resolution to new monitor

--- bin/vnc_office.sh
+++ bin/vnc_office.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -xeu
 vncserver -kill :1 -clean || echo 'there was no previous x'
-#vncserver -geometry 1920x1080 # desktop
-vncserver -geometry 1900x1060 # desktop, a bit of slack on the sides to fit with window borders
+vncserver -geometry 2540x1420
