a25b629d9f9fbf4a762691b1934c74d3c9ec1eae
Daniel Kuettel <dkuettel@gmail.com>
2017-06-17 22:48:26 +0200

    try to make more idempotent and failsafe

--- python.sh
+++ python.sh
@@ -5,6 +5,6 @@ sudo apt-get install -yq python python-pip build-essential
 sudo -H pip install --upgrade pip
 
 sudo apt-get install -yq ipython
-ipython profile create
+ipython profile create # todo doesn't work the second time, not quite idempotent
 # todo this way I will never see if there is a new default config file (new options)
-ln -s ~/config/python/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+ln -sb ~/config/python/ipython_config.py ~/.ipython/profile_default/ipython_config.py
