2a4b0c4c71df8b10d7a4140e16a60dbe18150f47
Daniel Kuettel <dkuettel@gmail.com>
2017-06-26 19:41:47 +0200

    try simple debug python for tmux+vim

---
+++ bin/jpython.py
@@ -0,0 +1,19 @@
+
+import exceptions
+import sys
+import traceback                                              
+import os
+import imp
+
+try:                                                               
+	execfile(sys.argv[1])
+except:                                                            
+	ex_type, ex, tb = sys.exc_info()                               
+	print ''.join(traceback.format_exception(ex_type, ex, tb))     
+	if isinstance(ex, exceptions.SyntaxError):
+		os.system('tmux split-window -h zsh -c "stty -ixon; vim +%d %s"' % (ex.lineno, ex.filename))
+	else:
+		locs = traceback.extract_tb(tb)                                
+		loc = locs[-1]                                                 
+		os.system('tmux split-window -h zsh -c "stty -ixon; vim +%d %s"' % (loc[1], loc[0]))
+	raise
