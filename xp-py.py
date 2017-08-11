1b4f78bbc0072d0a9e3dfcc311bd0eaab9e55a7a
Daniel Kuettel <dkuettel@gmail.com>
2017-08-11 15:15:26 +0200

    functions to divert mm

--- xp-py.py
+++ xp-py.py
@@ -11,3 +11,21 @@ from nn.xp_config import Struct
 if xpc.has():
 	c = xpc.load()
 import nn.nn_utils as nnu
+
+def divert_mm(modelfile='./model.pickle'):
+	import nn.fs
+	modelfile = nn.fs.realpath(modelfile)
+	m = xpc.load(modelfile)
+	m.instance = xm.get_local_instance()
+	m.instance.update()
+	xpc.save(m, modelfile)
+	return m
+
+
+def disable_mm(modelfile='./model.pickle'):
+	import nn.fs
+	modelfile = nn.fs.realpath(modelfile)
+	m = xpc.load(modelfile)
+	m.instance = None
+	xpc.save(m, modelfile)
+	return m
