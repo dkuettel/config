94fcb32cd6210023d1af8558c6741faf9e128ba3
Daniel Kuettel <dkuettel@gmail.com>
2017-08-25 10:33:27 +0200

    add headless dropbox

---
+++ dropbox.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh -eux
+set -o pipefail
+
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+wget 'https://www.dropbox.com/download?dl=packages/dropbox.py' ~/.dropbox-dist/dropbox.py
+chmod +x ~/.dropbox-dist/dropbox.py
+
+# run ~/.dropbox-dist/dropboxd to start the daemon
+# use dbox to start the python tool
