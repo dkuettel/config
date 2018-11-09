6d9d092f0d270f4861ba788a82b0d586195a585c
Daniel Kuettel <dkuettel@gmail.com>
2018-11-09 23:04:27 +0100

    lock and sleep command

--- keyboard/fn_keys.sh
+++ keyboard/fn_keys.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
+set -eux -o pipefail
 echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u -k all
 # needs a reboot after that to take effect
--- keyboard/install.sh
+++ keyboard/install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -eux
-set -o pipefail
+#!/bin/zsh
+set -eux -o pipefail
 
 sudo ln -sf ~/config/keyboard/pointluck /usr/share/X11/xkb/symbols/pointluck
 #sudo dpkg-reconfigure xkb-data # todo still necessary?
