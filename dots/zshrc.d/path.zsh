a77569d234e94bfe818daaf592ca7d1f5b31a9d7
Daniel Kuettel <dkuettel@gmail.com>
2019-05-14 14:47:11 +0200

    extend path

--- dots/zshrc.d/path.zsh
+++ dots/zshrc.d/path.zsh
@@ -3,6 +3,7 @@ typeset -U PATH path # array with no duplicates
 # note: ~ does not expand here, but $HOME does
 path=(
     $HOME/config/bin
+    $HOME/bin
     $HOME/fzf/bin
     $HOME/.local/bin
     /snap/bin $path
