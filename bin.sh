af1351d31b6e2fb4d9efb3a78ecdcddbefbcb7fc
Daniel Kuettel <dkuettel@gmail.com>
2019-05-19 17:19:04 +0200

    remove old stuff or add it to new config

--- bin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# todo nothing todo anymore because it's now in the path instead of a symlink
-#if [[ $(realpath ~/config/bin) == $(realpath ~/bin) ]]
-#then
-	#echo 'bin already installed'
-#else
-	#ln -s --backup ~/config/bin ~/bin
-#fi
--- vim/ftdetect/prototxt.vim
+++
@@ -1,2 +0,0 @@
-au BufRead,BufNewFile *.prototxt set filetype=prototxt
-au BufRead,BufNewFile *.proto set filetype=prototxt
