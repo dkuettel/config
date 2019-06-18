3b26f0c4cc23168f3282ad2cbc7d288681720785
Daniel Kuettel <dkuettel@gmail.com>
2019-06-18 20:24:55 +0200

    pipenv settings

--- dots/zshrc.d/experimental.zsh
+++ dots/zshrc.d/experimental.zsh
@@ -1,1 +1,8 @@
 source ~/toys/ecopy/widget.zsh
+
+# less fancy output
+export PIPENV_COLORBLIND=yes
+export PIPENV_HIDE_EMOJIS=yes
+# todo: there is also the --bare option, but no env variable for it
+# default python version for a new pipenv environment
+export PIPENV_DEFAULT_PYTHON_VERSION=3.7
