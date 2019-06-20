9b926805f0a4d3895b0617178f703352a2d29f29
Daniel Kuettel <dkuettel@gmail.com>
2019-06-20 13:45:25 +0200

    ncal alias

--- dots/zshrc.d/experimental.zsh
+++ dots/zshrc.d/experimental.zsh
@@ -6,3 +6,6 @@ export PIPENV_HIDE_EMOJIS=yes
 # todo: there is also the --bare option, but no env variable for it
 # default python version for a new pipenv environment
 export PIPENV_DEFAULT_PYTHON_VERSION=3.7
+
+# default ncal settings
+alias ncal='ncal -bM'
