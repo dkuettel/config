1c2fcf7c929a8cbe5819ce9636b0fa16509b7a8f
Daniel Kuettel <dkuettel@gmail.com>
2019-07-20 12:12:39 +0200

    fix losing dbox status

--- dots/tmux.conf.d/bin/status.py
+++ dots/tmux.conf.d/bin/status.py
@@ -3,46 +3,61 @@ import contextlib
 
 class status_dropbox:
     def __init__(self):
-        pass
+        import os
+
+        self.path = os.path.expanduser("~/.dropbox/command_socket")
 
     def __enter__(self):
-        import socket
-
-        self.s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.f = None
+        self.socket = None
+        self.stream = None
         return self.status
 
     def connect(self):
-        import os
+        import socket
 
         try:
-            self.s.connect(os.path.expanduser("~/.dropbox/command_socket"))
-            self.f = self.s.makefile("rw")
+            self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            self.socket.connect(self.path)
+            self.stream = self.socket.makefile("rw")
         except:
-            self.f = None
+            self.stream = None
+            self.socket = None
+            raise
 
     def status(self):
-        if self.f is None:
-            self.connect()
-        if self.f is None:
-            return None
+        if not self.stream:
+            try:
+                self.connect()
+            except FileNotFoundError:
+                return None
+            except ConnectionRefusedError:
+                return "?"
+
         try:
-            self.f.write("get_dropbox_status\ndone\n")
-            self.f.flush()
+            self.stream.write("get_dropbox_status\ndone\n")
+            self.stream.flush()
+
             idle = False
-            for l in self.f:
+            for l in self.stream:
                 if l == "done\n":
                     break
                 idle |= l == "status\tUp to date\n"
+
             return "✓" if idle else "x"
+
         except:
-            self.f = None
+            self.stream = None
+            self.socket = None
             return "?"
 
     def __exit__(self, *args):
-        if self.f:
-            self.f.close()
-        self.s.close()
+        if self.stream:
+            self.stream.close()
+        del self.stream
+
+        if self.socket:
+            self.socket.close()
+        del self.socket
 
 
 @contextlib.contextmanager
