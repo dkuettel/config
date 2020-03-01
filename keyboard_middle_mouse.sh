4d9da0f6e161624d25f479031b7be6f769779406
Daniel Kuettel <dkuettel@gmail.com>
2020-03-01 02:17:57 +0100

    remove old files

--- keyboard_middle_mouse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xeu
-set -o pipefail
-echo 'not completely automatic, better run it manually for now'
-# see http://linuxaleph.blogspot.ch/2008/11/mapping-middle-click-to-keyboard-key.html
-sudo apt-get install xkbset
-xkbset m # enable mouse actions
-xmodmap -e 'keycode 169 = Pointer_Button2' # map to mouse action, use xev for keycodes
