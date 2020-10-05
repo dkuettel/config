0c230ab6445f53182d40f5def5be9d88c4e41682
Daniel Kuettel <dkuettel@gmail.com>
2020-10-05 15:33:36 +0200

    make pipenv use local .venv folder

--- dots/zshrc.d/experimental.zsh
+++ dots/zshrc.d/experimental.zsh
@@ -1,11 +1,12 @@
 source ~/toys/ecopy/widget.zsh
 
-# less fancy output
+# pipenv sanity as far as it is possible
 export PIPENV_COLORBLIND=yes
 export PIPENV_HIDE_EMOJIS=yes
 # todo: there is also the --bare option, but no env variable for it
 # default python version for a new pipenv environment
 export PIPENV_DEFAULT_PYTHON_VERSION=3.8
+export PIPENV_VENV_IN_PROJECT=1
 
 # default ncal settings
 alias ncal='ncal -bM'
