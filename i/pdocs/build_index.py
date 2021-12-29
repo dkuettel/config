aec657ae3f1474de20918aa8e460f98f076b461e
Daniel Kuettel <dkuettel@gmail.com>
2021-12-29 18:41:36 +0100

    update and adapt to new pdocs

---
+++ i/pdocs/build_index.py
@@ -0,0 +1,51 @@
+import abc
+import base64
+import builtins
+import collections
+import concurrent.futures
+import contextlib
+import dataclasses
+import datetime
+import faulthandler
+import functools
+import hashlib
+import inspect
+import io
+import itertools
+import json
+import math
+import multiprocessing as mp
+import os
+import pathlib
+import pickle
+import queue
+import random
+import re
+import shutil
+import socket
+import stat
+import statistics
+import subprocess
+import sys
+import threading
+import time
+import traceback
+import typing
+import warnings
+from pathlib import Path
+
+import boto3
+import botocore
+import click
+import numpy as np
+import pandas as pd
+import skimage.io
+import skimage.transform
+import tensorflow as tf
+import yaml
+from numpy import ndarray as A
+from pandas import DataFrame as DF
+
+import pdocs
+
+pdocs.write_index()
