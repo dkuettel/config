ed29f814cc622666bc8c85cb0fa393b6bceb7b44
Daniel Kuettel <dkuettel@gmail.com>
2022-01-09 23:17:43 +0100

    reproduce solarized colors from derivation

--- colors/colors.py
+++ colors/colors.py
@@ -1,7 +1,10 @@
 import numpy as np
 import skimage
+from tabulate import tabulate
 
 # see https://terminalguide.namepad.de/
+# see http://www.easyrgb.com/en/math.php
+# http://www.brucelindbloom.com/index.html?ColorCalculator.html
 # see http://misc.flogisoft.com/bash/tip_colors_and_formatting
 # see http://ethanschoonover.com/solarized
 
@@ -167,10 +170,15 @@ for i in range(4):
     # ]
 
 
-def hex_to_sgr(hex):
+def hex_to_rgb(hex):
     r = (hex & 0xFF0000) >> 16
     g = (hex & 0x00FF00) >> 8
     b = (hex & 0x0000FF) >> 0
+    return (r, g, b)
+
+
+def hex_to_sgr(hex):
+    r, g, b = hex_to_rgb(hex)
     return f"{r};{g};{b}"
 
 
@@ -178,6 +186,9 @@ def validate_solarized(
     named_colors=solarized_light_named_colors,
     named_indices=solarized_light_named_indices,
 ):
+    print()
+    print("validate solarized theme")
+
     bases = ["base03", "base02", "base01", "base00", "base0", "base1", "base2", "base3"]
     opposites = dict(zip(bases[:4], bases[4:])) | dict(zip(bases[4:], bases[:4]))
     print(
@@ -211,12 +222,132 @@ def validate_solarized(
         )
     )
 
-    # TODO
-    # show 16 basecolors once using direct once using indexed
-    # this way you can check if you terminal is configured correctly
-    # which convention to use? could be parametrized though, default as I did, as solarized I think wanted it
-    # also, if using direct, does asciinema respect that? would make it easier
-    # could use tmux for making those videos
+
+def hex_to_lab(hex):
+    rgb = (
+        np.array([(hex & 0xFF0000) >> 16, (hex & 0x00FF00) >> 8, (hex & 0x0000FF) >> 0])
+        / 255.0
+    )
+    lab = skimage.color.rgb2lab(rgb, illuminant="D65", observer="2")
+    return lab
+
+
+# TODO why not rgb_from_lab(lab)
+def lab_to_rgb(lab):
+    lab = np.array(lab).astype(float)  # row vector
+    xyz = skimage.color.lab2xyz(lab, illuminant="D50", observer="2")
+    # http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
+    # surprisingly, this is not there in skimage
+    von_kries_D50_to_D65 = np.array(
+        [
+            [0.9845002, -0.0546158, 0.0676324],
+            [-0.0059992, 1.0047864, 0.0012095],
+            [0.0, 0.0, 1.3194581],
+        ]
+    )
+    xyz = xyz @ von_kries_D50_to_D65.T
+    rgb = skimage.color.xyz2rgb(xyz)
+    return rgb * 255.0
+
+
+solarized_dark_named_labs = dict(
+    base03=(15, -12, -12),
+    base02=(20, -12, -12),
+    base01=(45, -7, -7),
+    base00=(50, -7, -7),
+    base0=(60, -6, -3),
+    base1=(65, -5, -2),
+    base2=(92, -0, 10),
+    base3=(97, 0, 10),
+    yellow=(60, 10, 65),
+    orange=(50, 50, 55),
+    red=(50, 65, 45),
+    magenta=(50, 65, -5),
+    violet=(50, 15, -45),
+    blue=(55, -10, -45),
+    cyan=(60, -35, -5),
+    green=(60, -20, 65),
+)
+
+
+def sgr_from_lab(lab):
+    # r, g, b = (lab_to_rgb(lab) * 255).round().astype(int)
+    r, g, b = (lab_to_rgb(lab)).round().astype(int)
+    return f"{r};{g};{b}"
+
+
+def testing_solarized(
+    named_colors=solarized_dark_named_colors, named_labs=solarized_dark_named_labs
+):
+
+    # TODO https://github.com/altercation/solarized#the-values
+    # says white D65 and reference D50, not totally clear to me what is what
+    # I think D50 is for CIELAB, the original values
+    # and D65 is then for sRGB, that means you need to apply chromatic adaptation
+    # von kries gives almost the same results os in his table, but not fully
+    # blue is quite a bit off in numbers, but maybe overall it's close enough
+
+    # TODO maybe an opportunity for me to change the colors that are difficult, orange<>red and violet<>blue ?
+    # make a matrix to let me see each combination and if they are visible
+
+    print()
+    print("test solarized derivation")
+
+    print(
+        tabulate(
+            [
+                (
+                    c,
+                    f"0x{named_colors[c]:06x}",
+                    *zip(tuple(hex_to_lab(named_colors[c]).round().astype(int)), lab),
+                    # *zip(hex_to_rgb(named_colors[c]), tuple(lab_to_rgb(lab))),
+                    "<->",
+                    *zip(
+                        hex_to_rgb(named_colors[c]),
+                        # tuple(lab_to_rgb(hex_to_lab(named_colors[c]))),
+                        tuple(lab_to_rgb(lab).round().astype(int)),
+                    ),
+                )
+                for c, lab in named_labs.items()
+            ],
+            headers=("name", "hex", "L*", "a*", "b*", "<->", "r", "g", "b"),
+        )
+    )
+
+    print()
+    bases = ["base03", "base02", "base01", "base00", "base0", "base1", "base2", "base3"]
+    opposites = dict(zip(bases[:4], bases[4:])) | dict(zip(bases[4:], bases[:4]))
+    print(
+        "    given bases: "
+        + " ".join(
+            f"[0;38;2;{hex_to_sgr(named_colors[opposites[base]])};48;2;{hex_to_sgr(named_colors[base])}m{base:8}[0m"
+            for base in bases
+        )
+    )
+    print(
+        "  derived bases: "
+        + " ".join(
+            f"[0;38;2;{sgr_from_lab(named_labs[opposites[base]])};48;2;{sgr_from_lab(named_labs[base])}m{base:8}[0m"
+            for base in bases
+        )
+    )
+
+    print()
+    accents = ["yellow", "orange", "red", "magenta", "violet", "blue", "cyan", "green"]
+    print(
+        "  given accents: "
+        + " ".join(
+            f"[0;38;2;{hex_to_sgr(named_colors['base03'])};48;2;{hex_to_sgr(named_colors[acc])}m{acc:8}[0m"
+            for acc in accents
+        )
+    )
+    print(
+        "derived accents: "
+        + " ".join(
+            f"[0;38;2;{sgr_from_lab(named_labs['base03'])};48;2;{sgr_from_lab(named_labs[acc])}m{acc:8}[0m"
+            for acc in accents
+        )
+    )
 
 
 # TODO also see if we can reproduce the colors using CIELAB as described?
@@ -224,6 +355,8 @@ def validate_solarized(
 # or for randomization
 # then check base16 if that convention there works, to have access to more themes easily
 
+# TODO use tabulate for easier layouts? does it work with escape codes?
+# TODO does asciicinema respect 24bit colors? then there are no theme problems anymore for demos, possibly need to run tmux after for that?
 
 if __name__ == "__main__":
     # TODO use click to make it easier?
@@ -232,3 +365,4 @@ if __name__ == "__main__":
     # show_24bit_colors()
     # show_attributes()
     validate_solarized()
+    testing_solarized()
