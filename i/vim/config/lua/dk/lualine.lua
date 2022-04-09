586e3d91bedf994ad49bbdbd2b0e94ed59b7c196
Daniel Kuettel <dkuettel@gmail.com>
2022-04-09 19:57:33 +0200

    refactor lualine setup

--- i/vim/config/lua/dk/lualine.lua
+++ i/vim/config/lua/dk/lualine.lua
@@ -1,70 +1,36 @@
 local M = {}
 
 function M.setup()
-    -- https://github.com/kyazdani42/nvim-web-devicons
-    -- also requires patched fonts, like nerdfonts
-    vim.cmd("packadd nvim-web-devicons")
+    -- dependencies
+    do
+        -- https://github.com/kyazdani42/nvim-web-devicons
+        -- also requires patched fonts, like nerdfonts
+        vim.cmd("packadd nvim-web-devicons")
+    end
 
     -- https://github.com/nvim-lualine/lualine.nvim
     vim.cmd("packadd lualine.nvim")
 
-    local function show_window()
-        return "藍" .. vim.api.nvim_win_get_number(0)
-    end
-
-    -- zen low-flicker indication of file status (unsaved, saved, read-only)
-    local U = require("lualine.utils.utils")
-    local show_file_icons = { missing = "", modified = "", unmodified = "", read_only = "" }
-    local function show_file()
-        local icon = nil
-        if vim.bo.modifiable then
-            if vim.fn.filereadable(vim.fn.expand("%")) == 1 then
-                if vim.bo.modified then
-                    icon = show_file_icons.modified
-                else
-                    icon = show_file_icons.unmodified
-                end
-            else
-                icon = show_file_icons.missing
-            end
-        else
-            icon = show_file_icons.read_only
-        end
-        -- based on https://github.com/nvim-lualine/lualine.nvim/blob/master/lua/lualine/components/filename.lua
-        local name = U.stl_escape(vim.fn.expand("%:t"))
-        return icon .. " " .. name
-    end
-
-    -- zen low-flicker indication of lsp activity (no lsp, busy lsp, idle lsp)
-    local lsp_activity_icons = { missing = "", busy = "", idle = "" }
-    local function lsp_activity()
-        if #vim.lsp.get_active_clients() == 0 then
-            return lsp_activity_icons.missing
-        end
-        if #vim.lsp.util.get_progress_messages() == 0 then
-            return lsp_activity_icons.idle
-        end
-        return lsp_activity_icons.busy
-    end
-
     require("lualine").setup({
         options = {
-            theme = "auto", -- TODO how to keep in sync with themes?
+            -- see https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+            -- "auto" uses vim.g.colors_name, usually what's set with :colorscheme
+            theme = "auto",
             icons_enabled = true,
             component_separators = "",
             section_separators = { left = "", right = "" },
-            globalstatus = false, -- true not working yet?
+            globalstatus = false, -- could be cool, but looks broken when I try it
         },
         sections = {
-            lualine_a = { show_window, show_file },
+            lualine_a = { M.show_window, M.show_file },
             lualine_b = { { "diff", icon = "", colored = false } },
             lualine_c = {},
             lualine_x = {},
             lualine_y = { { "diagnostics", sources = { "nvim_lsp" }, colored = false } },
-            lualine_z = { lsp_activity, { "filetype", icons_enabled = false } },
+            lualine_z = { M.show_lsp_activity, { "filetype", icons_enabled = false } },
         },
         inactive_sections = {
-            lualine_a = { show_window, show_file },
+            lualine_a = { M.show_window, M.show_file },
             lualine_b = {},
             lualine_c = {},
             lualine_x = {},
@@ -83,6 +49,44 @@ function M.setup()
         },
         extensions = {},
     })
+end
+
+function M.show_window()
+    return "藍" .. vim.api.nvim_win_get_number(0)
+end
+
+-- zen low-flicker indication of file status (unsaved, saved, read-only)
+M.file_icons = { missing = "", modified = "", unmodified = "", read_only = "" }
+function M.show_file()
+    local icon = nil
+    if vim.bo.modifiable then
+        if vim.fn.filereadable(vim.fn.expand("%")) == 1 then
+            if vim.bo.modified then
+                icon = M.file_icons.modified
+            else
+                icon = M.file_icons.unmodified
+            end
+        else
+            icon = M.file_icons.missing
+        end
+    else
+        icon = M.file_icons.read_only
+    end
+    -- based on https://github.com/nvim-lualine/lualine.nvim/blob/master/lua/lualine/components/filename.lua
+    local name = require("lualine.utils.utils").stl_escape(vim.fn.expand("%:t"))
+    return icon .. " " .. name
+end
+
+-- zen low-flicker indication of lsp activity (no lsp, busy lsp, idle lsp)
+M.lsp_activity_icons = { missing = "", busy = "", idle = "" }
+function M.show_lsp_activity()
+    if #vim.lsp.get_active_clients() == 0 then
+        return M.lsp_activity_icons.missing
+    end
+    if #vim.lsp.util.get_progress_messages() == 0 then
+        return M.lsp_activity_icons.idle
+    end
+    return M.lsp_activity_icons.busy
 end
 
 return M
