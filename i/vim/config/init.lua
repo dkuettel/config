eed5928a18446cf890a83b139cab03111faa7fc2
Daniel Kuettel <dkuettel@gmail.com>
2022-04-21 22:00:32 +0200

    init.lua with a cleaner function maybe for a reloading setup eventually

--- i/vim/config/init.lua
+++ i/vim/config/init.lua
@@ -1,51 +1,48 @@
 local function setup(name)
     -- TODO wrap it an let it fail to still have a functional vim?
+    -- maybe reloading could sometimes work when using dofile instead of require?
     return require("dk." .. name).setup
 end
 
--- TODO put it in a load function, easier for reload
+local function load()
+    setup("basics")()
 
-setup("basics")()
+    -- themes
+    -- TODO themes also try to set cursor colors, but my alacritty, tmux, or term vim doesnt react to that?
+    setup("gruvbox")("light") -- could gruvbox material be interesting?
+    -- setup("tender")()
+    -- setup("tokyonight")("dark")
+    -- setup("everforest")("light")
+    -- setup("tender")()
+    -- setup("nord")()
+    -- setup("solarized")("light")
+    -- setup("nightfox")("nordfox")
 
--- themes
--- TODO themes also try to set cursor colors, but my alacritty, tmux, or term vim doesnt react to that?
-setup("gruvbox")("light") -- could gruvbox material be interesting?
--- setup("tender")()
--- setup("tokyonight")("dark")
--- setup("everforest")("light")
--- setup("tender")()
--- setup("nord")()
--- setup("solarized")("light")
--- setup("nightfox")("nordfox")
+    -- setup("auto-pairs")()
+    -- setup("vim-surround")()
+    setup("autosave")()
+    setup("treesitter")()
+    setup("telescope")()
+    setup("lualine")()
+    -- setup("coc")()
+    setup("null-ls")()
+    setup("lspconfig")()
+    setup("hop")()
+    setup("gitsigns")()
+    setup("comment")()
+    setup("neogit")()
 
--- setup("auto-pairs")()
--- setup("vim-surround")()
-setup("autosave")()
-setup("treesitter")()
-setup("telescope")()
-setup("lualine")()
--- setup("coc")()
-setup("null-ls")()
-setup("lspconfig")()
-setup("hop")()
-setup("gitsigns")()
-setup("comment")()
-setup("neogit")()
+    -- TODO playing
+    setup("funky-format")()
+    -- TODO cannot use relative path
+    vim.keymap.set(
+        "n",
+        ",R",
+        dofile("/home/dkuettel/config/i/vim/config/lua/dk/funky-format.lua").setup,
+        { desc = "reload funky formatter" }
+    )
 
--- TODO playing
-setup("funky-format")()
--- TODO cannot use relative path
-vim.keymap.set(
-    "n",
-    ",R",
-    dofile("/home/dkuettel/config/i/vim/config/lua/dk/funky-format.lua").setup,
-    { desc = "reload funky formatter" }
-)
+    require("dk.basics").active_window()
+end
 
-require("dk.basics").active_window()
-
--- TODO doesnt quite work I think because
--- a) doesnt reload "requires"
--- b) not sure if all plugin setup()s can be used more than once
--- vim.cmd("command! Reload source $MYVIMRC | echo 'config reloaded'")
--- vim.keymap.set("n", ",R", "<cmd>Reload<enter>")
+load()
