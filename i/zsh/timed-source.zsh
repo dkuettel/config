8e7c3fbb9dd5391ad8fd3f68fe1e84c0c9f68a10
Daniel Kuettel <dkuettel@gmail.com>
2022-08-08 17:59:56 +0200

    zsh timed source formats paths nicer

--- i/zsh/timed-source.zsh
+++ i/zsh/timed-source.zsh
@@ -5,7 +5,7 @@ source $1
 _source_end=$EPOCHREALTIME
 
 ((_source_duration = (_source_end-_source_start) * 1000))
-printf "%5.1fms for $1\n" $_source_duration
+printf "%5.1fms for ${(D)1}\n" $_source_duration
 ((_source_total = _source_total + _source_duration))
 
 unset _source_start _source_end _source_duration
