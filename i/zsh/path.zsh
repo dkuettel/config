c70f076b8cab883f28335536dcd4092001367c14
Daniel Kuettel <dkuettel@gmail.com>
2022-08-08 15:10:40 +0200

    path.zsh shorter and no leaked variables

--- i/zsh/path.zsh
+++ i/zsh/path.zsh
@@ -1,20 +1,14 @@
 
-typeset -U PATH path  # array with no duplicates
-
-# note: we reverse, append, and reverse
-# together with unique this adds only what's not already there
-typeset -U rev
-rev=(${(Oa)path})
-
-# note: ~ does not expand here, but $HOME does
-rev+=(
-    /snap/bin  # should be there from system settings?
-    $HOME/.local/bin  # pip install puts executables here
-    #$HOME/fzf/bin  # not needed anymore, installed with apt now
-    $HOME/.cargo/bin  # rust toolchain
-    $HOME/bin/nodejs/bin
-    $HOME/bin
+path=(
     $HOME/config/bin
+    $HOME/bin
+    $HOME/bin/nodejs/bin
+    $HOME/.cargo/bin  # rust toolchain
+    $HOME/.local/bin  # pip install puts executables here
+    /snap/bin  # should be there from system settings?
+    $path
 )
 
-path=(${(Oa)rev})
+# path -> reverse -> keep only first unique -> reverse
+# prevents any duplication for paths that are already setup
+path=(${(Oa)${(u)${(Oa)path}}})
