21df0e2d284a65f474b917d6c0abbd37e4e2113d
Daniel Kuettel <dkuettel@gmail.com>
2022-08-16 23:10:34 +0200

    retire AutoSave.nvim plugin and use built-in stuff

--- i/vim/config/lua/dk/autosave.lua
+++ i/vim/config/lua/dk/autosave.lua
@@ -1,47 +1,32 @@
 local M = {}
 
-function M.setup()
-    -- https://github.com/Pocco81/AutoSave.nvim
-    -- also seems to check for new content on FocusGained
-    vim.cmd("packadd AutoSave.nvim")
-
-    -- controls CursorHold event delay
-    -- but also other things, plus some completion plugins set that too
-    -- not sure it will persist
-    vim.opt.updatetime = 500
-
-    require("autosave").setup({
-        execution_message = "",
-        -- FocusLost doesnt seem to trigger
-        events = { "InsertLeave", "TextChanged", "CursorHold", "CursorHoldI", "FocusLost" },
-        debounce_delay = 500,
-    })
+local function callback(context)
+    if string.find(context.file, "^/efs/") ~= nil then
+        return
+    end
+    -- update() only saves if the file has been modified, no-op otherwise
+    vim.cmd.update()
 end
 
-function M.legacy()
-    vim.cmd([[
-        """ autosave and -read
-        " interesting events:
-        " InsertLeave, TextChanged, CursorHold
-        " TextChangedI, CursorHoldI
-        " FocusGained, FocusLost
-        " (needs events configured coming from the terminal or tmux, check if that works)
-        set autoread
-        set updatetime=500
-        augroup autosave
-            autocmd!
-            autocmd InsertLeave,TextChanged * silent! w
-            autocmd CursorHold,CursorHoldI * silent! update
-            autocmd FocusLost * silent! wa
-            autocmd FocusGained * silent! checktime
-        augroup END
-        " problems:
-        " should not run :w when buffer has no file
-        " silent! surpressed that error messages
-        " but would be nicer not to try at all
-        " CursorHoldI doesn't seem to trigger update currently
-        " how to enable/disable when editing slow files (efs), 'autocmd! autosave' disables, but now way to get back
-    ]])
+function M.setup()
+    -- controls CursorHold and CursorHoldI events
+    -- (idle time before they are triggered in milliseconds)
+    -- might want to check that it's not set to a longer value again later
+    vim.opt.updatetime = 500
+
+    -- checks if files have changed on disk, especially on FocusGained
+    vim.opt.autoread = true
+
+    -- interesting events:
+    --   InsertLeave, TextChanged, CursorHold
+    --   TextChangedI, CursorHoldI, but TextChangedI is on every keystroke
+    --   FocusGained, FocusLost (needs terminal or tmux to be configured to send those escape codes)
+    local events = { "InsertLeave", "TextChanged", "CursorHold", "CursorHoldI", "FocusLost", "FocusGained" }
+
+    vim.api.nvim_create_autocmd(events, {
+        desc = "autosave",
+        callback = callback,
+    })
 end
 
 return M
