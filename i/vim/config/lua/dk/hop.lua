6989721baee6efd9c3428fd9a6090ff1a6eeb792
Daniel Kuettel <dkuettel@gmail.com>
2022-12-15 14:34:50 +0100

    vim hop colors

--- i/vim/config/lua/dk/hop.lua
+++ i/vim/config/lua/dk/hop.lua
@@ -12,10 +12,18 @@ function M.setup()
         -- but signs are attached to a buffer, not to a window
         -- commented there with a possible solution
         char2_fallback_key = "<enter>",
+        jump_on_sole_occurrence = false,
     })
 
     vim.keymap.set("n", "s", hop.hint_char1, { desc = "hop char1" })
     vim.keymap.set("n", "S", hop.hint_char2, { desc = "hop char2" })
+
+    vim.cmd([[
+        highlight! link HopUnmatched EndOfBuffer
+        highlight! HopNextKey gui=bold guibg=#3c3836 guifg=#fbf1c7  " inverse normal
+        highlight! HopNextKey1 gui=bold,underline guifg=#3c3836 guibg=#fbf1c7  " normal
+        highlight! HopNextKey2 gui=underline guifg=#3c3836 guibg=#fbf1c7  " normal
+    ]])
 end
 
 function M.legacy()
