a961f8cb09d29421a23dc3ba5a98332c01e98d0a
Daniel Kuettel <dkuettel@gmail.com>
2023-04-12 16:55:39 +0200

    yaml formatter proposal (yves?)

--- i/vim/config/lua/dk/funky-formatter.lua
+++ i/vim/config/lua/dk/funky-formatter.lua
@@ -8,6 +8,7 @@ function M.setup()
             python = { command = { "isort-and-black" } },
             lua = { command = { "some-stylua" } },
             json = { command = { "jq" } },
+            -- yaml = { command = { "prettier", "--parser", "yaml" } },
             rust = { command = { "rustfmt" } },
             markdown = { command = { "pandoc", "--from=markdown", "--to=markdown" } },
             gitignore = { command = { "env", "-", "LC_ALL=C", "sort", "--unique" } },
